b780609e5b32f8f2e2ffb71d8d428e91747f1872
Korovin Viktor <sarvash.inc@gmail.com>
2020-06-19 01:10:56 +0700

    Add more test for lexer

--- core/lexer/tests/test_parse.py
+++ core/lexer/tests/test_parse.py
@@ -4,17 +4,29 @@ from core import Atom, SourceStr, Lexer
 from core import LexerKekException
 
 
-@pytest.mark.parametrize(
-    'raw, expected',
-    (
-        ("a", "a"),
-        ('abcde', 'abcde'),
-        ('(a)', ('a', )),
-        ('(a b c d)', ('a', 'b', 'c', 'd')),
-        ('(x y (z w))', ('x', 'y', ('z', 'w'))),
-        ('(x a(ii mm(a b)x)b)', ('x', 'a', ('ii', 'mm', ('a', 'b'), 'x'), 'b')),
-        ('   (\ta\t\t\t  \n  b c \n\n\nd\t\t)', ('a', 'b', 'c', 'd'))
-    ))
+checks = (
+    ("", None),
+    ("(a b c)", ('a', 'b', 'c')),
+    ("( a b c)", ('a', 'b', 'c')),
+    ("(a b c )", ('a', 'b', 'c')),
+    ("(a b    c)", ('a', 'b', 'c')),
+    ("(    a     b       c   )", ('a', 'b', 'c')),
+    ("(a \nb    \n c\n\n\n\n)\n", ('a', 'b', 'c')),
+    ("(a (b c))", ('a', ('b', 'c'))),
+    ("((a b) (c d (e f)))", (('a', 'b'), ('c', 'd', ('e', 'f')))),
+    ("(abcdef abcdef)", ("abcdef", "abcdef")),
+    ("(quotes here \" \')", ('quotes', 'here', '"', "'")),
+    ("a", "a"),
+    ('abcde', 'abcde'),
+    ('(a)', ('a', )),
+    ('(a b c d)', ('a', 'b', 'c', 'd')),
+    ('(x y (z w))', ('x', 'y', ('z', 'w'))),
+    ('(x a(ii mm(a b)x)b)', ('x', 'a', ('ii', 'mm', ('a', 'b'), 'x'), 'b')),
+    ('   (\ta\t\t\t  \n  b c \n\n\nd\t\t)', ('a', 'b', 'c', 'd'))
+)
+
+
+@pytest.mark.parametrize('raw, expected', checks)
 def test_parse_good(raw: str, expected: Atom):
     source = SourceStr(raw, name='<test>')
     lexer = Lexer(source)
@@ -28,10 +40,10 @@ def test_parse_good(raw: str, expected: Atom):
 @pytest.mark.parametrize(
     'raw, expected',
     (
-        ('(a', None),
-        ('(a (b', None),
-        ('(x ))', None),
-        (')', None),
+            ('(a', None),
+            ('(a (b', None),
+            ('(x ))', None),
+            (')', None),
     )
 )
 def test_parse_wrong(raw, expected):
